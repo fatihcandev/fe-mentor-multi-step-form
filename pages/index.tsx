bc349fc55d745b0ddc9234ef81dc6e29adcd5780
Fatih <facan@cisco.com>
2023-01-23 13:47:04 +0300

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import { FormProvider } from 'react-hook-form'
 import { useForm } from '@/hooks'
 import Layout from '@/components/Layout'
+import Head from 'next/head'
 
 export default function Home() {
   const form = useForm()
   return (
-    <FormProvider {...form}>
-      <Layout />
-    </FormProvider>
+    <>
+      <Head>
+        <title>Frontend Mentor | Multi-step Form with Next.js</title>
+      </Head>
+      <FormProvider {...form}>
+        <Layout />
+      </FormProvider>
+    </>
   )
 }
